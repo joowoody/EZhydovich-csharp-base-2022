9ddfde95bdfafd7800d59185e3710b43fa1aa859
Euheni Zhydovich <44315444+joowoody@users.noreply.github.com>
2022-05-16 18:06:18 +0300

    Fix

---
+++ HomeWork/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+<
+HomeWork.sln,a/4/a44e8ac24f5650bd76afc63d54c31a4b3692de1b
+R
+"HelloWorld1-1/HelloWorld1-1.csproj,d/4/d4839610bc38a6e9d13ae1527354231f98fc15b8
+H
+HelloWorld1-1/Program.cs,c/a/caa64310d896ecefe2931584a113e184570d0a60
--- HomeWork1-2/VariableHomework/Task1/Program.cs
+++ HomeWork1-2/VariableHomework/Task1/Program.cs
@@ -1,1 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// See https://aka.ms/new-console-template for more information]
+internal static class Task1
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("enter x: ");
+        var x = int.Parse(Console.ReadLine());
+        Console.WriteLine("enter y: ");
+        var y = int.Parse(Console.ReadLine());
+        Console.WriteLine("enter z: ");
+        var z = int.Parse(Console.ReadLine());
+        var resultOfFunction = (x + y + z) * (x - z) / (y * y) + 2 * z;
+
+        Console.WriteLine($"x={x}  y={y}    z={z}");
+        Console.WriteLine(resultOfFunction);
+        Console.ReadKey();
+
+    }
+}
